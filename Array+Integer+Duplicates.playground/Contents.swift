ac5afd542d541a7963a4e72f1e4dff40decabb2f
Ruslan L. Soldatenko <ruslan@qualtie.com>
2025-02-25 18:17:16 +0300

    improve containsDuplicate

--- Array+Integer+Duplicates.playground/Contents.swift
+++ Array+Integer+Duplicates.playground/Contents.swift
@@ -1,14 +1,7 @@
 import Foundation
 
 func containsDuplicate(_ nums: [Int]) -> Bool {
-    var appears = Set<Int>()
-    for num in nums {
-        if appears.contains(num) {
-            return true
-        }
-        appears.insert(num)
-    }
-    return false
+    Set(nums).count != nums.count
 }
 
 print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
