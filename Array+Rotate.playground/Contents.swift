027a522c47c3bb5570206fc54b62d8f581e772d0
Ruslan L. Soldatenko <ruslan@qualtie.com>
2025-03-03 16:09:20 +0300

    add alt rotate method

--- Array+Rotate.playground/Contents.swift
+++ Array+Rotate.playground/Contents.swift
@@ -6,6 +6,17 @@ func rotate(_ nums: inout [Int], _ k: Int) {
     }
 }
 
+func rotate2(_ nums: inout [Int], _ k: Int) {
+    let k = k % nums.count
+    nums.reverse()
+    nums[..<k].reverse()
+    nums[k...].reverse()
+}
+
 var nums = [1, 2, 3, 4, 5, 6, 7]
 rotate(&nums, 3)
 print(nums)
+
+var nums2 = [1, 2, 3, 4, 5, 6, 7]
+rotate2(&nums2, 3)
+print(nums2)
