b7f24dfcf4f865cf54f1fc0dab8c1140e90599fa
Ruslan L. Soldatenko <soldatenko@inbox.ru>
2025-03-12 12:33:49 +0300

    add intersect

--- Array+Intersection.playground/Contents.swift
+++ Array+Intersection.playground/Contents.swift
@@ -4,4 +4,20 @@ func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
     Array(Set(nums1).intersection(Set(nums2)))
 }
 
-print(intersection([1, 2, 2, 1], [2, 2]))
+func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    var freq = [Int: Int](), result = [Int]()
+    for num in nums1 {
+        freq[num, default: 0] += 1
+    }
+    for num in nums2 {
+        if let value = freq[num], value > 0 {
+            result.append(num)
+            freq[num]! -= 1
+        }
+    }
+    return result
+}
+
+print(intersection([1, 2, 2, 1], [2, 2])) // [2]
+print(intersect([1, 2, 2, 1], [2, 2])) // [2, 2]
+
