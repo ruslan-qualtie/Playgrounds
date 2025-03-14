dd81e4fcd8bb024ea0425f9722d96c80005348a9
Ruslan L. Soldatenko <ruslan@qualtie.com>
2025-03-14 15:06:33 +0300

    add String+RemoveDuplicates

---
+++ String+RemoveDuplicates.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func removeDuplicates(_ s: String) -> String {
+    var stack: [Character] = []
+    for char in s {
+        if !stack.isEmpty && stack.last == char {
+            stack.removeLast()
+        } else {
+            stack.append(char)
+        }
+    }
+    return String(stack)
+}
+
+print(removeDuplicates("abbaca"))  // ➝ "ca"
+print(removeDuplicates("azxxzy"))  // ➝ "ay"
+print(removeDuplicates("a"))       // ➝ "a"
+print(removeDuplicates("aaaa"))    // ➝ ""
+print(removeDuplicates("abab"))
