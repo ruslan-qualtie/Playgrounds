ea8545c8687974c2d77a9647133d906e0491178f
Ruslan L. Soldatenko <soldatenko@inbox.ru>
2025-03-31 12:42:46 +0300

    add String+GetEncryptedString

--- String+GetEncryptedString+3210.playground/Contents.swift
+++ String+GetEncryptedString+3210.playground/Contents.swift
@@ -1,3 +1,15 @@
-import UIKit
+// 3210. Find the Encrypted String
+// https://leetcode.com/problems/find-the-encrypted-string/
 
-var greeting = "Hello, playground"
+import Foundation
+
+func getEncryptedString(_ s: String, _ k: Int) -> String {
+    var result = "", sArr = Array(s), n = sArr.count
+    for i in 0..<n {
+        result.append(sArr[(i + k) % n])
+    }
+    return result
+}
+
+print(getEncryptedString("dart", 3)) // tdar
+print(getEncryptedString("aaa", 1)) // aaa
