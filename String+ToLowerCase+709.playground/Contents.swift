b6b68e9442c0cf67ce6618407a32c22db8b70792
Ruslan L. Soldatenko <ruslan@qualtie.com>
2025-04-11 11:41:51 +0300

    add String+ToLowerCase+709

---
+++ String+ToLowerCase+709.playground/Contents.swift
@@ -0,0 +1,12 @@
+// 709. To Lower Case
+// https://leetcode.com/problems/to-lower-case/
+
+import Foundation
+
+func toLowerCase(_ s: String) -> String {
+    s.lowercased()
+}
+
+print(toLowerCase("Hello")) // "hello"
+print(toLowerCase("here")) // "here"
+print(toLowerCase("LOVELY")) // "lovely"
