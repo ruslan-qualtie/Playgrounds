d3a302a1e698cd9bc306466f429d32bd3eff9f75
Ruslan L. Soldatenko <soldatenko@inbox.ru>
2025-04-25 14:29:24 +0300

    add Array+String+FirstPalindrome+2108

--- Array+String+FirstPalindrome+2108.playground/Contents.swift
+++ Array+String+FirstPalindrome+2108.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+// 2108. Find First Palindromic String in the Array
+// https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
 
-var greeting = "Hello, playground"
+
+import Foundation
+
+func firstPalindrome(_ words: [String]) -> String {
+    for word in words {
+        if isPalindrome(word) {
+            return word
+        }
+    }
+    return ""
+}
+private func isPalindrome(_ s: String) -> Bool {
+    var sarr = Array(s), left = 0, right = sarr.count - 1
+    while left < right {
+        if sarr[left] == sarr[right] {
+            left += 1
+            right -= 1
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+print(firstPalindrome(["abc","car","ada","racecar","cool"])) // "ada"
+print(firstPalindrome(["notapalindrome","racecar"])) // "racecar"
+print(firstPalindrome(["def","ghi"])) // ""
